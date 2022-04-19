353021f26f574c74de42f2dad1410a70416e153f
Dejan Trajkovic <dtrajko@gmail.com>
2022-04-20 01:38:39 +0200

    Creating an Ethereum wallet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,23 @@
 require("@nomiclabs/hardhat-waffle");
 
+const fs = require("fs");
+const privateKey = fs.readFileSync(".secret").toString();
+
+// const projectId = "********************************";
+
 module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: process.env.REACT_APP_INFURA_POLYGON_MUMBAI_RPC_URL,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: process.env.REACT_APP_INFURA_POLYGON_MAINNET_RPC_URL,
+      accounts: [privateKey]
+    }
+  },
   solidity: "0.8.4",
 };
