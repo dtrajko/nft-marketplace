1347c6a48802301eb683058d489d22598bbb4c40
Dejan Trajkovic <dtrajko@gmail.com>
2022-04-20 08:08:06 +0200

    Completing the NFTMarket smart contract

---
+++ test/NFTMarketTest.js
@@ -0,0 +1,8 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("NFTMarket", function () {
+    it ("Should create and execute market sales", async function () {
+        
+    });
+});
